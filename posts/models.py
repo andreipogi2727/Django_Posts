4ce433b7e1f2e7ed76baab23179f860558bf1c1e
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-04-01 22:07:12 -0400

    Added a class called Post in my posts models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import Profile
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank = True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
