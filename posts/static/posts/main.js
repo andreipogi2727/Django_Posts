5a6fe36961b129fd244ec6edf44a9d196ea6a7e1
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-04-02 02:36:39 -0400

    Added the basic logic for unlike and like. It displays how many liked and displays like or unlike according to the people liked

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -28,7 +28,7 @@ const getData = () =>{
                                     <div class="row>
                                         <div class ="col-1">
                                             <a href="#" class="btn btn-primary">Details</a>
-                                            <a href="#" class="btn btn-primary">Like</a>
+                                            <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
                                         </div>
                                     </div>
                                 </div>
