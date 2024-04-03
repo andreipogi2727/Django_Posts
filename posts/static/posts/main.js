cf4476dcb5e9960551812e168ccd02efd4f44d34
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-04-03 02:30:54 -0400

    Fixed drozone and adding posts bugs

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -35,6 +35,13 @@ const getCookie =(name)=> {
     return cookieValue;
 }
 const csrftoken = getCookie('csrftoken');
+
+const deleted = localStorage.getItem('title')
+if (deleted){
+    handleAlerts('danger', `delete "${deleted}"`)
+    localStorage.clear()
+}
+
 let visible = 3
 
 const likeUnlikePosts = ()=> {
@@ -90,7 +97,7 @@ const getData = () =>{
                                         </div>
                                         <div class ="col-2">
                                             <form class="like-unlike-forms" data-form-id="${el.id}">
-                                                <button href="#" class="btn btn-primary" id ="like-unlike-${el.id}"> ${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                                <button class="btn btn-primary" id ="like-unlike-${el.id}"> ${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
                                             </form>
                                         </div>
                                     </div>
@@ -123,6 +130,7 @@ loadBtn.addEventListener('click', ()=>
     getData()
 })
 
+let newPostId = null
 postForm.addEventListener('submit', e=>{
     e.preventDefault()
 
@@ -136,6 +144,7 @@ postForm.addEventListener('submit', e=>{
         },
         success: function(response){
             console.log(response)
+            newPostId = response.id
             postsBox.insertAdjacentHTML('afterbegin', `
                 <div class="card mb-2">
                 <div class="card-body">
@@ -145,11 +154,11 @@ postForm.addEventListener('submit', e=>{
                     <div class="card-footer">
                         <div class="row">
                             <div class ="col-2">
-                                <a href="#" class="btn btn-primary">Details</a>
+                                <a href="${url}${response.id}" class="btn btn-primary">Details</a>
                             </div>
                             <div class ="col-2">
                                 <form class="like-unlike-forms" data-form-id="${response.id}">
-                                    <button href="#" class="btn btn-primary" id ="like-unlike-${response.id}">Like (0)</button>
+                                    <button  class="btn btn-primary" id ="like-unlike-${response.id}">Like (0)</button>
                                 </form>
                             </div>
                         </div>
@@ -181,7 +190,23 @@ closeBtns.forEach(btn=> btn.addEventListener('click', ()=>{
     if (!dropzone.classList.contains('not-visible')){
         dropzone.classList.add('not-visible')
     }
+    const myDropzone = Dropzone.forElement("#my-dropzone")
+    myDropzone.removeAllFiles(true)
 }))
+
+dropzone.autoDiscover = false
+const myDropzone = new Dropzone('#my-dropzone', {
+    url: 'upload/',
+    init: function(){
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 5,
+    maxFilesSize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
+})
 
 getData()
 
--- posts/urls.py
+++ posts/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('upload/', image_upload_view, name="image-upload"),
     path('<pk>/', post_detail, name='post-detail'),
     path('<pk>/update/', update_post, name='post-update'),
-    path('<pk>/delete', delete_post, name='post-delete'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
     path('data/<int:num_posts>/', load_posts_data_view, name='posts-data'),
     path('<pk>/data/', post_detail_data_view, name='posts-detail-data'),
 
